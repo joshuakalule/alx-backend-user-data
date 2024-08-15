9a916599b86a0a689e4048407d06439c07cbba80
Joshua Kalule <kaljoshkalule@gmail.com>
2024-08-15 10:53:15 +0300

    add checkpw

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -7,3 +7,8 @@ import bcrypt
 def hash_password(password: str) -> bytes:
     """Hash a password using bcrypt"""
     return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """validates the provided password"""
+    return bcrypt.checkpw(password.encode(), hashed_password)
