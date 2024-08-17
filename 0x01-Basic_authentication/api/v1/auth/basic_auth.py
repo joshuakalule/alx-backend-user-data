f795094b5f65216b38cdcb28ed57908066d443cd
Joshua Kalule <kaljoshkalule@gmail.com>
2024-08-17 17:47:16 +0300

    add retrieval of a user

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,7 +5,8 @@ Module to handle Basic authentication
 import binascii
 from api.v1.auth.auth import Auth
 import base64
-from typing import Tuple
+from typing import Tuple, TypeVar
+from models.user import User
 
 
 class BasicAuth(Auth):
@@ -50,3 +51,20 @@ class BasicAuth(Auth):
             return None, None
         email, password = decoded_base64_authorization_header.split(':')
         return email, password
+
+    def user_object_from_credentials(self,
+                                     user_email: str,
+                                     user_pwd: str) -> TypeVar('User'): # type: ignore
+        """Fetches user instance based on email and password"""
+        if not user_email or not user_pwd:
+            return None
+        if not isinstance(user_email, str) or not isinstance(user_pwd, str):
+            return None
+
+        found_users = User.search({'email': user_email})
+        if len(found_users) == 0:
+            return None
+        user: User = found_users[0]
+        if not user.is_valid_password(user_pwd):
+            return None
+        return user
