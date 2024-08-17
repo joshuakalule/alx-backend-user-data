7da7c82f6b7736857234ea545b0f635383cb8560
Joshua Kalule <kaljoshkalule@gmail.com>
2024-08-17 06:29:50 +0300

    implement method to extract authorization parameters for basic auth

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -7,4 +7,14 @@ from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     """Class to handle basic authentication."""
-    pass
+
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """Retrieves the autho-params of the Authurization header"""
+        if not authorization_header:
+            return None
+        if type(authorization_header) != str:
+            return None
+        if not authorization_header.startswith('Basic '):
+            return None
+        return authorization_header.replace('Basic ', '').strip()
