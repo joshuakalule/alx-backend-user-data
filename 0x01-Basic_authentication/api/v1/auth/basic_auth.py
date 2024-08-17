589c1655684267d958d50267cb01fce87c28d523
Joshua Kalule <kaljoshkalule@gmail.com>
2024-08-17 06:47:32 +0300

    implement base64 decoding of authurization header parameters

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,7 +2,9 @@
 """
 Module to handle Basic authentication
 """
+import binascii
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -11,6 +13,7 @@ class BasicAuth(Auth):
     def extract_base64_authorization_header(self,
                                             authorization_header: str) -> str:
         """Retrieves the autho-params of the Authurization header"""
+
         if not authorization_header:
             return None
         if type(authorization_header) != str:
@@ -18,3 +21,18 @@ class BasicAuth(Auth):
         if not authorization_header.startswith('Basic '):
             return None
         return authorization_header.replace('Basic ', '').strip()
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header: str
+                                           ) -> str:
+        """Decodes auth-param for basic auth"""
+
+        if not base64_authorization_header:
+            return None
+        if type(base64_authorization_header) != str:
+            return None
+        try:
+            b = base64.b64decode(base64_authorization_header, validate=True)
+            return b.decode('utf-8')
+        except binascii.Error:
+            return None
