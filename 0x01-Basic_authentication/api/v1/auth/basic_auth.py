77f3b426633af898ff214e8a0ac47afc1422195c
Joshua Kalule <kaljoshkalule@gmail.com>
2024-08-17 06:22:51 +0300

    add auth method 'Basic'

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Module to handle Basic authentication
+"""
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """Class to handle basic authentication."""
+    pass
