8e9633dc8c0ee42917d103322e2828df7a9b0b78
Joshua Kalule <kaljoshkalule@gmail.com>
2024-08-19 05:18:13 +0300

    add method to get user_id based on session_id

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -13,13 +13,18 @@ class SessionAuth(Auth):
 
     def create_session(self, user_id: str = None) -> str:
         """Creates a session id for user_id"""
-        if not user_id:
-            return None
-        if type(user_id) != str:
+        if not user_id or type(user_id) != str:
             return None
 
-        session_id = uuid4()
+        session_id = str(uuid4())
 
         self.user_id_by_session_id[session_id] = user_id
 
         return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """Retrieves a user_id based on session_id."""
+        if not session_id or type(session_id) != str:
+            return None
+
+        return self.user_id_by_session_id.get(session_id, None)
