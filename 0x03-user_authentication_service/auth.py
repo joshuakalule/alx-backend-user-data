0738f46f59a7845610f927c315a5beac1dad45cf
Joshua Kalule <kaljoshkalule@gmail.com>
2024-08-21 12:28:31 +0300

    add a method to validate credentials

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Module that handles authentication."""
 
-from bcrypt import hashpw, gensalt
+from bcrypt import hashpw, gensalt, checkpw
 from db import DB
 from user import User
 from sqlalchemy.orm.exc import NoResultFound
@@ -20,6 +20,17 @@ class Auth:
 
     def __init__(self):
         self._db = DB()
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """Returns True if email exists and is associated with password"""
+        try:
+            found_user = self._db.find_user_by(email=email)
+        except Exception:
+            return False
+
+        bytes_password = password.encode('utf-8')
+
+        return checkpw(bytes_password, found_user.hashed_password)
 
     def register_user(self, email: str, password: str) -> User:
         """Register a user."""
