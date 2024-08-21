daab4206fc9b507ddfe681cfa3e7f6a156d0dda4
Joshua Kalule <kaljoshkalule@gmail.com>
2024-08-21 13:07:23 +0300

    add route for login

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,10 +2,33 @@
 """Basic Flask app."""
 
 from auth import Auth
-from flask import Flask, jsonify, request
+from flask import abort, Flask, jsonify, request
 
 AUTH = Auth()
 app = Flask(__name__)
+
+
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login():
+    """End point for loging in"""
+
+    email = request.form.get('email')
+    password = request.form.get('password')
+    if not email or not password:
+        abort(401)
+
+    if not AUTH.valid_login(email, password):
+        abort(401)
+
+    session_id = AUTH.create_session(email)
+
+    payload = {
+        'email': email,
+        "message": "logged in"
+    }
+    response = jsonify(payload)
+    response.set_cookie('session_id', session_id)
+    return response, 200
 
 
 @app.route('/users', methods=['POST'], strict_slashes=False)
